5a9efe330db47796539a1c1fafd49d1ed0f718b2
Marcos Silvano <marcossilvano@gmail.com>
2020-11-18 07:33:19 -0300

    Exercicio tabuada 17-11-2020

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercicio_tabuada_17_11_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            int resultado;
+
+            Console.Write("Digite o número da tabuada desejada: ");
+            numero = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            for(int x = 0; x <= 10; x++)
+            {
+                resultado = numero * x;
+                Console.WriteLine($"{x} X {numero} = {resultado}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
